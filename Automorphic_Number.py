ea8d2dc064b571a1a800ea39357dba361909677e
lalithasri-30 <121622819+lalithasri-30@users.noreply.github.com>
2023-06-06 23:59:36 +0530

    import math n=int(input()) c=0 sqr=n*n temp=n while temp>0:     c+=1     temp=temp//10; flag=math.floor(pow(10,c)) last=sqr%flag if(last==n):     print("Automorphic Number") else:     print("Not an Automorphic Number")

---
+++ Automorphic_Number.py
@@ -0,0 +1,15 @@
+import math
+n=int(input())
+c=0
+sqr=n*n
+temp=n
+while temp>0:
+    c+=1
+    temp=temp//10;
+flag=math.floor(pow(10,c))
+last=sqr%flag
+if(last==n):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
+    
