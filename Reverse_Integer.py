4514588fea7088c6d42886102299a3adec0a3e6c
lalithasri-30 <121622819+lalithasri-30@users.noreply.github.com>
2023-06-06 23:04:55 +0530

    n=int(input()) s=0 negitive=False if n<0:     negitive=True     n=abs(n) while n!=0:     r=n%10     s=s*10+r     n=n//10 if negitive:     s=-s print(s)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,13 +1,13 @@
-def reverse(n):
-    s=0
-    while n!=0:
-        i = n%10
-        s=s*10+i
-        n=n//10
-    return s
 n=int(input())
-if n>0:
-    print(reverse(n))
-else:
-    n=n*-1
-    print(-1*reverse(n))
+s=0
+negitive=False
+if n<0:
+    negitive=True
+    n=abs(n)
+while n!=0:
+    r=n%10
+    s=s*10+r
+    n=n//10
+if negitive:
+    s=-s
+print(s)
